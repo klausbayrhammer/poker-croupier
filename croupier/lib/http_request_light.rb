57e64ca2dae7f520942d989747d13c5e9b4e67fb
devill <devillmeister@gmail.com>
2014-12-04 09:54:10 +0100

    Increased timeout for result delivery, and decreased for bet requests

--- croupier/lib/http_request_light.rb
+++ croupier/lib/http_request_light.rb
@@ -6,24 +6,24 @@ class Croupier::HttpRequestLight
     @host, @port, @path = uri.host, uri.port, uri.path
   end
 
-  def post(message, &block)
+  def post(message, time_out, &block)
     begin
-      try_request(message, &block)
+      try_request(message, time_out, &block)
     rescue Exception => e
       yield true, { message: e.message, code: nil } if block_given?
     end
   end
 
   class << self
-    def post(url, message, &block)
-      self.new(url).post(message, &block)
+    def post(url, message, time_out = 0.5, &block)
+      self.new(url).post(message, time_out, &block)
     end
   end
 
   private
 
-  def try_request(message)
-    response = build_http_connection.start do |http|
+  def try_request(message, time_out)
+    response = build_http_connection(time_out).start do |http|
       http.request(build_request(message))
     end
 
@@ -36,10 +36,10 @@ class Croupier::HttpRequestLight
     req
   end
 
-  def build_http_connection
+  def build_http_connection(time_out)
     http_connection = Net::HTTP.new(@host, @port)
-    http_connection.open_timeout = 2
-    http_connection.read_timeout = 2
+    http_connection.open_timeout = time_out
+    http_connection.read_timeout = time_out
     http_connection
   end
 end
