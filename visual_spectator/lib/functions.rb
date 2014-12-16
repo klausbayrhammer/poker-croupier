d0d4ee60de805a3a99fb28a52233f665c0a8f6e4
devill <devillmeister@gmail.com>
2014-12-16 17:58:23 +0100

    Remove deprecated visual spectator

--- visual_spectator/lib/functions.rb
+++
@@ -1,3 +0,0 @@
-def strip_path_and_extension(file)
-  File.dirname(file).split('/')[-1] + "/" + File.basename(file, ".*")
-end
--- visual_spectator/visual_spectator.rb
+++
@@ -1,9 +0,0 @@
-$:.push(File.join(File.dirname(__FILE__), 'lib/api'))
-$:.push(File.join(File.dirname(__FILE__)))
-
-module VisualSpectator
-  autoload :Game, 'lib/game'
-  autoload :Player, 'lib/player'
-  autoload :Tournament, 'lib/tournament'
-  autoload :Twitter, 'lib/twitter'
-end
